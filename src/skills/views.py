2e3fa205b7c54fd8b98b4e9724e2fc6270b45187
Killian Morere <killian.morere@fms-ea.com>
2026-03-09 11:24:41 +0100

    Added homepage view.

--- src/skills/views.py
+++ src/skills/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+from .models import Profile
+
+
+class HomeView(generic.ListView):
+    template_name = "skills/home.html"
+    context_object_name = "profiles"
+
+    def get_queryset(self):
+        return Profile.objects.all()
