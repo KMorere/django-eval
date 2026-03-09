1d3ff871b07e18f9d02a5cbb7cf26bf7d1d956d5
Killian Morere <killian.morere@fms-ea.com>
2026-03-09 11:11:07 +0100

    Added admin page setup and custom display.

--- src/skills/admin.py
+++ src/skills/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Activity, Profile, Skill
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "profiles"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInLine]
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ["skill_name"]
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ["activity_date", "needed_skill", "target_user", "active_user"]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Activity, ActivityAdmin)
