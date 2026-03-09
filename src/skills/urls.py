6688f2557d79dd3c1b4b1f97c10f1e2a22daedbf
Killian Morere <killian.morere@fms-ea.com>
2026-03-09 11:50:28 +0100

    Added view for all skills.

--- src/skills/urls.py
+++ src/skills/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "skills"
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="home")
+    path("", views.HomeView.as_view(), name="home"),
+    path("all/", views.SkillView.as_view(), name="all"),
 ]
--- src/skills/views.py
+++ src/skills/views.py
@@ -3,12 +3,22 @@ from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 
-from .models import Profile
+from .models import Profile, Skill
 
 
 class HomeView(generic.ListView):
+    model = Profile
     template_name = "skills/home.html"
     context_object_name = "profiles"
 
     def get_queryset(self):
         return Profile.objects.all()
+
+
+class SkillView(generic.ListView):
+    model = Skill
+    template_name = "skills/skills.html"
+    context_object_name = "skills"
+
+    def get_queryset(self):
+        return Skill.objects.all().order_by("skill_name").values()
